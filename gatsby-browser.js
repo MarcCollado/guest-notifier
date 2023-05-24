101d4abbec185e228225b5dd4f386b1e5e38b2e0
Marc Collado <marc@collado.io>
2023-05-24 18:03:52 +0200

    Import global layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
+import React from 'react';
+import Layout from './src/components/layout';
+import './src/styles/main.css';
 
-// You can delete this file if you're not using it
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>;
+}
