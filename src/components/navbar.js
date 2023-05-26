895313ede6b71a8245565023c8b74835cdf4806d
Marc Collado <marc@collado.io>
2023-05-26 18:48:53 +0200

    Use nav instead of header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 
-import User from '/src/icons/user.svg';
-import * as styles from '/src/styles/navbar.module.css';
+import User from '../icons/user.svg';
+import * as styles from '../styles/navbar.module.css';
 
 const Navbar = ({ siteTitle }) => (
-  <header className={styles.container}>
+  <nav className={styles.container}>
     <Link to="/">
-      <p className="large">{siteTitle}</p>
+      <p className="large">
+        <b>{siteTitle}</b>
+      </p>
     </Link>
     <User />
-  </header>
+  </nav>
 );
 
 export default Navbar;
