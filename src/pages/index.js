4ce89d75e991151337ee9eddade2cd6e7e094399
Marc Collado <marc@collado.io>
2023-05-29 14:36:39 +0200

    Create guests boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,18 @@ import * as React from 'react';
 
 import Hero from '../components/hero';
 import Seo from '../components/seo';
+import GuestCard from '../components/guest-card';
+
 import * as styles from '/src/styles/index.module.css';
+
+const renderGuests = (numOfGuests) => {
+  return Array.from({ length: numOfGuests }, (_, i) => <GuestCard key={i} />);
+};
 
 const IndexPage = () => (
   <>
     <Hero />
+    <div className={styles.featuredGuestsContainer}>{renderGuests(8)}</div>
   </>
 );
 
