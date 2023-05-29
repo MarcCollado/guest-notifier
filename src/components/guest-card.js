97b5b951a361a6b5e0ab1762211997d49a210e78
Marc Collado <marc@collado.io>
2023-05-29 14:36:53 +0200

    Create GuestCard component

---
+++ src/components/guest-card.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+import * as styles from '../styles/guest-card.module.css';
+
+const GuestCard = ({}) => <div className={`center ${styles.container}`}></div>;
+
+export default GuestCard;
