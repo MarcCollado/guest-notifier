f7141409db12c1158d45333ef238443e61eb98fd
Marc Collado <marc@collado.io>
2023-05-31 16:30:46 +0200

    Use ref instead of anchors

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,29 +1,37 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
 import GuestCard from './guest-card';
 import * as styles from '../styles/carousel.module.css';
 
-const renderCarouselItems = (carouselId, numOfItems) => {
-  return Array.from({ length: numOfItems }, (_, i) => (
-    <GuestCard key={i + 1} href={`${carouselId}-${i + 1}`}>
-      {`API item #${i + 1}`}
-    </GuestCard>
+const renderCarouselItems = (id, n, onRenderItem) => {
+  return Array.from({ length: n }, (_, i) => (
+    <GuestCard key={i} onRenderItem={onRenderItem} index={i}>{`API item #${
+      i + 1
+    }`}</GuestCard>
   ));
 };
 
-const renderCarouselNav = (carouselId, numOfItems) => {
-  return Array.from({ length: numOfItems }, (_, i) => (
-    <a href={`#${carouselId}-${i + 1}`}></a>
+const renderCarouselNav = (id, n, onClick) =>
+  Array.from({ length: n }, (_, i) => (
+    <a key={i + 1} onClick={() => onClick(i)}></a>
   ));
-};
 
 const Carousel = ({ items, id }) => {
+  const [refList, setRefList] = useState([]);
+
+  const onRenderItem = (obj) => refList.push(obj) && setRefList(refList);
+
+  const onClick = (index) => {
+    const found = refList.find((refObj) => refObj.index === index);
+    found && found.ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className={styles.carouselContainer}>
       <div className={styles.carouselItems}>
-        {renderCarouselItems(id, items)}
+        {renderCarouselItems(id, items, onRenderItem)}
       </div>
-      {renderCarouselNav(id, items)}
+      {renderCarouselNav(id, items, onClick)}
     </div>
   );
 };
--- src/components/guest-card.js
+++ src/components/guest-card.js
@@ -1,11 +1,24 @@
-import * as React from 'react';
+import React, { forwardRef, useEffect, useRef } from 'react';
 
 import * as styles from '../styles/guest-card.module.css';
 
-const GuestCard = (props) => (
-  <div className={`center ${styles.guestCardContainer}`} id={props.href}>
-    {props.children}
-  </div>
-);
+const GuestCard = forwardRef((props, ref) => {
+  const itemRef = useRef(null);
+
+  useEffect(() => {
+    if (!!itemRef) {
+      props.onRenderItem({
+        index: props.index,
+        ref: itemRef,
+      });
+    }
+  }, [itemRef]);
+
+  return (
+    <div className={`center ${styles.guestCardContainer}`} ref={itemRef}>
+      {props.children}
+    </div>
+  );
+});
 
 export default GuestCard;
