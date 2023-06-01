aaace2677633c50c9fcc1d13841e990ee5895d4d
Marc Collado <marc@collado.io>
2023-06-01 14:16:05 +0200

    Rename functions

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,12 +3,11 @@ import React, { useState } from 'react';
 import GuestCard from './guest-card';
 import * as styles from '../styles/carousel.module.css';
 
-const renderCarouselItems = (n, onRenderItem) => {
+const renderCarouselItems = (n, addItemToRefList) => {
   return Array.from({ length: n }, (_, i) => (
     <GuestCard
       key={i}
-      index={i}
-      onRenderItem={onRenderItem}
+      onRenderItem={addItemToRefList}
     >{`API item #${i}`}</GuestCard>
   ));
 };
@@ -21,17 +20,17 @@ const renderCarouselNav = (n, onClick) =>
 const Carousel = ({ items }) => {
   const [refList, setRefList] = useState([]);
 
-  const updateRefList = (obj) => refList.push(obj) && setRefList(refList);
+  const refListGenerator = (obj) => refList.push(obj) && setRefList(refList);
 
   const onClick = (index) => {
-    const found = refList.find((refObj) => refObj.index === index);
+    const found = refList.find((refObj) => refObj.position === index);
     found && found.ref.current.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div className={styles.carouselContainer}>
       <div className={styles.carouselItems}>
-        {renderCarouselItems(items, updateRefList)}
+        {renderCarouselItems(items, refListGenerator)}
       </div>
       {renderCarouselNav(items, onClick)}
     </div>
