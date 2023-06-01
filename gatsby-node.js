9e8d1349fc0bbb96c7052a5097c5595a3ffe5814
Marc Collado <marc@collado.io>
2023-06-01 17:20:04 +0200

    Source guests from REST API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
@@ -11,4 +13,31 @@ exports.createPages = async ({ actions }) => {
   });
 };
 
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  const guestList = await fetch('https://api.guestnotifier.com/v1.0/guests')
+    .then((res) => res.json())
+    .catch((error) => console.error('Error:', error));
+  guestList.guests.map((g) => {
+    createNode({
+      // GraphQL required fields
+      id: `${g.id}`,
+      parent: null,
+      internal: {
+        type: `Guests`, // graphQL query: allGuests { ... }
+        contentDigest: createContentDigest(g),
+      },
+      children: [],
+      // API fields
+      name: g.name,
+      surname: g.surname,
+    });
+    return;
+  });
+};
+
 // 📍 https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+// 📍 https://www.gatsbyjs.com/docs/reference/config-files/actions/#createNode
+// 📍 https://www.gatsbyjs.com/docs/tutorial/creating-a-source-plugin/#create-source-plugin
