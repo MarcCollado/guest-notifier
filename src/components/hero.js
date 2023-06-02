41fa9352cfdb5395756f4d767d11249680aa3c69
Marc Collado <marc@collado.io>
2023-06-02 17:00:31 +0200

    Source img with StaticImage API

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,24 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import * as styles from '../styles/hero.module.css';
 
-const Hero = ({}) => (
-  <div className={`center ${styles.container}`}>
-    <h1 className={`display ${styles.siteTitle}`}>
-      Stay up-to-date with the latest podcast buzz!
-    </h1>
-    <p className={`${styles.tagline}`}>Get a weekly newsletter for you</p>
-  </div>
-);
+const Hero = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.imageContainer}>
+        <StaticImage
+          src="../images/hero-avatar.png"
+          alt="Hero Avatar"
+          placeholder="blurred"
+          width={260}
+        />
+      </div>
+      <h1 className={`display ${styles.siteTitle}`}>
+        Stay up-to-date with the latest podcast buzz!
+      </h1>
+    </div>
+  );
+};
 
 export default Hero;
