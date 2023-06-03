d4ed73134993157b9ae6ee547d4188ef0bf2a2fe
Marc Collado <marc@collado.io>
2023-06-03 20:38:28 +0200

    Add logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,30 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import User from '../icons/user.svg';
 import * as styles from '../styles/navbar.module.css';
 
-const Navbar = ({ siteTitle }) => (
-  <nav className={styles.container}>
+const Navbar = () => (
+  <nav className={styles.navbarContainer}>
+    <div className={styles.logoContainer}>
+      <StaticImage
+        src="../images/logo.png"
+        alt="Guest Notifier logo"
+        placeholder="blurred"
+        width={44}
+      />
+      {window.matchMedia('(min-width: 768px)').matches && (
+        <StaticImage
+          src="../images/brand.png"
+          alt="Guest Notifier"
+          placeholder="blurred"
+          width={236}
+        />
+      )}
+    </div>
     <Link to="/">
-      <p className="large">
-        <b>{siteTitle}</b>
-      </p>
+      <p className="large"></p>
     </Link>
     <User />
   </nav>
