543e5316d72b25f95e62e962bc39c22fb8cd9916
Marc Collado <marc@collado.io>
2023-06-04 09:57:45 +0200

    Use GatsbyImage to render logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,57 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 import User from '../icons/user.svg';
 import * as styles from '../styles/navbar.module.css';
 
-const Navbar = () => (
-  <nav className={styles.navbarContainer}>
-    <div className={styles.logoContainer}>
-      <StaticImage
-        src="../images/logo.png"
-        alt="Guest Notifier logo"
-        placeholder="blurred"
-        width={44}
-      />
-      {window.matchMedia('(min-width: 768px)').matches && (
-        <StaticImage
-          src="../images/brand.png"
-          alt="Guest Notifier"
-          placeholder="blurred"
-          width={236}
-        />
-      )}
-    </div>
-    <Link to="/">
-      <p className="large"></p>
-    </Link>
-    <User />
-  </nav>
-);
+const Navbar = () => {
+  const data = useStaticQuery(graphql`
+    query ImagesQuery {
+      logoImage: allFile(
+        filter: { absolutePath: { regex: "/images/logo.png/" } }
+      ) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData(
+              width: 44
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+      }
+      textImage: allFile(
+        filter: { absolutePath: { regex: "/images/guest-notifier.png/" } }
+      ) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData(
+              width: 245
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+      }
+    }
+  `);
+
+  const logoImage = data.logoImage.nodes[0].childImageSharp.gatsbyImageData;
+  const textImage = data.textImage.nodes[0].childImageSharp.gatsbyImageData;
+
+  return (
+    <nav className={styles.navbarContainer}>
+      <div className={styles.logoContainer}>
+        <GatsbyImage image={logoImage}></GatsbyImage>
+        <GatsbyImage image={textImage}></GatsbyImage>
+      </div>
+      <Link to="/">
+        <p className="large"></p>
+      </Link>
+      <User />
+    </nav>
+  );
+};
 
 export default Navbar;
